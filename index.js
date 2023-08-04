d419b8fcd7013f0b4eadcd4d25b4337af9d8a691
Duong Thanh Toan <thanhtoan0306@gmail.com>
2023-08-04 17:00:17 +0000

    Update tasks.json and index.js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("Hello CodeSandbox");
+const http = require("http");
+
+const hostname = "localhost";
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.setHeader("Content-Type", "text/html");
+  res.send(`
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>Hello, World!</title>
+      </head>
+      <body>
+        <h1>Hello, World!</h1>
+      </body>
+    </html>
+  `);
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server listening on port ${port}`);
+});
